7287feb474123ecba1faec6b9953d480682d8048
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-12-22 17:14:46 +0200

    changes to default export

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -2,10 +2,12 @@ import { getIsLoggedIn, getIsRefreshing } from 'redux/auth/selectors';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
   const isRefreshing = useSelector(getIsRefreshing);
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
+
+export default PrivateRoute;
--- src/components/Routes/RestrictedRoute.js
+++ src/components/Routes/RestrictedRoute.js
@@ -2,7 +2,9 @@ import { getIsLoggedIn } from 'redux/auth/selectors';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
+
+export default RestrictedRoute;
